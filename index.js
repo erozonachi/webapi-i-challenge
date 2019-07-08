882772242b99b346ee9c83ad85150b43515796d9
erozonachi <erozonachi.james@gmail.com>
2019-07-08 14:52:36 +0100

    Spin up a server with express module

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+const port = 5000;
+
+//Routes...
+app.get('/', (req, res) => {
+  res.status(200).json('The very begining of this API service...');
+});
+
+app.listen(port, () => {
+  console.log('Server running on port: 5000');
+});
