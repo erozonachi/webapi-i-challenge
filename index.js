53a37b62533d74274a8c8818d207c5dfcbad48fc
erozonachi <erozonachi.james@gmail.com>
2019-07-08 15:06:49 +0100

    Create get all users endpoint

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // implement your API here
 const express = require('express');
+const Users = require('./data/db');
 
 const app = express();
 app.use(express.json());
 const port = 5000;
 
 //Routes...
-app.get('/', (req, res) => {
-  res.status(200).json('The very begining of this API service...');
+app.get('/api/users', (req, res) => { //Get all users
+  Users.find()
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(err => {
+    res.status(500).json({ error: "The users information could not be retrieved." });
+  });
 });
 
 app.listen(port, () => {
